fbbbef589bb0fea1af7c384d581cf897e4047ced
Jorge Rodríguez Fernández <jorge.rodriguezfernandez.ext@telefonicaed.com>
2017-08-09 11:33:04 +0200

    Cambios logs por sysos, para que salga por consola. Capturada excepcion si no existe el administrador de cursos.

--- docroot/WEB-INF/src/com/liferay/lms/upgrade/UpgradeTo3_1_1.java
+++ docroot/WEB-INF/src/com/liferay/lms/upgrade/UpgradeTo3_1_1.java
@@ -6,8 +6,6 @@ import java.util.List;
 import com.liferay.lms.model.Module;
 import com.liferay.lms.service.ModuleLocalServiceUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.ResourceConstants;
@@ -18,7 +16,6 @@ import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.service.RoleLocalServiceUtil;
 
 public class UpgradeTo3_1_1 extends UpgradeProcess {
-	Log log = LogFactoryUtil.getLog(UpgradeTo3_1_1.class);
 	public int getThreshold() {
 		return 211;
 	}
@@ -27,26 +24,29 @@ public class UpgradeTo3_1_1 extends UpgradeProcess {
 		// your upgrade code here.
 		
 		 Role siteMember;
-	     
-	    log.warn("--- UPGRADING LMS TO 3.1.1 ");
 	    
-	    log.warn("--- SETTING ACCESS PERMISSION TO COURSE ADMINISTRATION");
+		System.out.println("--- UPGRADING LMS TO 3.1.1 ");
+	    System.out.println("--- SETTING ACCESS PERMISSION TO COURSE ADMINISTRATION");
 	    List<Company> companys = CompanyLocalServiceUtil.getCompanies();
 	    for(Company company : companys){
-	    	  Role courseAdministrator = RoleLocalServiceUtil.getRole(company.getCompanyId(), "Administrador de cursos");
-	    	  if(courseAdministrator!=null){
-	    		  ResourcePermissionLocalServiceUtil.setResourcePermissions(company.getCompanyId(), 
-			     			Module.class.getName(),ResourceConstants.SCOPE_COMPANY, String.valueOf(company.getCompanyId()),  courseAdministrator.getRoleId(),  new String[]{"ACCESS"});
-	    	  }
+	    	 //try{
+	    		 Role courseAdministrator = RoleLocalServiceUtil.getRole(company.getCompanyId(), "Administrador dess cursos");
+		    	 if(courseAdministrator!=null){
+		    		  ResourcePermissionLocalServiceUtil.setResourcePermissions(company.getCompanyId(), 
+				     			Module.class.getName(),ResourceConstants.SCOPE_COMPANY, String.valueOf(company.getCompanyId()),  courseAdministrator.getRoleId(),  new String[]{"ACCESS"});
+		    	  }
+	    	/* }catch(Exception e){
+	    		 e.printStackTrace();
+	    	 }*/
 	    			  
 	    }
 	    
 	    
-	    log.warn("--- SETTING ACCESS PERMISSION TO MODULES ");
+	    System.out.println("--- SETTING ACCESS PERMISSION TO MODULES ");
 	    List<Module> modules = ModuleLocalServiceUtil.getModules(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 		for(Module module : modules){
 			try{
-				log.warn("--- MODULE  "+ module.getModuleId());
+				System.out.println("--- MODULE  "+ module.getModuleId());
 			    siteMember = RoleLocalServiceUtil.fetchRole(module.getCompanyId(), RoleConstants.SITE_MEMBER);
 				if(siteMember!=null){
 					ResourcePermissionLocalServiceUtil.setResourcePermissions(module.getCompanyId(), 
@@ -56,6 +56,6 @@ public class UpgradeTo3_1_1 extends UpgradeProcess {
 				e.printStackTrace();
 			}
 		}
-		
+		 System.out.println("--- END SETTING ACCESS PERMISSION TO MODULES ");
 	}
 }
